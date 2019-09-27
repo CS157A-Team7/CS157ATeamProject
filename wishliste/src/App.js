0d1bb6930f0386c1f86dd9c1a4b25a4a02864b0b
francis-sjsu123 <47190615+francis-sjsu123@users.noreply.github.com>
2019-09-26 18:46:32 -0700

    Updated App to display data grab from database

--- wishliste/src/App.js
+++ wishliste/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    results: []
+  };
+
+  componentDidMount() {
+    const url = '/api/test.php'
+    axios.get(url).then(response => response.data)
+    .then((data) => {
+      this.setState({ results: data })
+      console.log(this.state.results)
+     })
+  };
+
+
+  render() {
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table border='1' width='100%'>
+        <tr>
+            <td>{ this.state.results}</td>
+        </tr>
+      </table>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
