f074104a1bee3528d3952bad4ad45d5b07236c7a
francis-sjsu123 <47190615+francis-sjsu123@users.noreply.github.com>
2019-09-26 18:49:55 -0700

    Added new Build files

---
+++ wishliste/build/precache-manifest.2dc888c0e8c0252c1d4b5a98351c8322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e98f984d82553083e7888ad5e6b461",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cf2896322ae97df66c0",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0c6d3c91da88fd1383da",
+    "url": "./static/js/2.f21ac5d5.chunk.js"
+  },
+  {
+    "revision": "9cf2896322ae97df66c0",
+    "url": "./static/js/main.c49f4a9b.chunk.js"
+  },
+  {
+    "revision": "2212feaaf58ff2a4b114",
+    "url": "./static/js/runtime-main.e40a85a2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
