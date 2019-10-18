3b3c91e74cc73da9b6f1948304f2cb24a44174e3
Holly Lind <hollylind0@gmail.com>
2019-10-18 09:29:23 -0700

    Made components for displaying lists

---
+++ wishliste/src/AllLists.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import './App.css';
+import List from './List.js';
+
+const ListRow = props => {
+  var lists = [];
+  for (var i = props.startIndex; i <= props.endIndex; i++) {
+    lists.push(<List listData={props.allLists[i]} />)
+  }
+
+  return (
+    <header className="List-row">
+      {lists}
+    </header>  
+  )
+}
+
+class AllLists extends Component {
+  render() {
+    const { allLists } = this.props
+
+    var everything = [];
+    var counter = 0;
+    var moreToDraw = true;
+    while (moreToDraw) {
+      if (allLists.length - counter > 4) {
+        everything.push(<ListRow allLists={allLists} startIndex={counter} endIndex={counter+3} />)
+        counter += 4;
+      }
+      else {
+        everything.push(<ListRow allLists={allLists} startIndex={counter} endIndex={allLists.length-1} />)
+        moreToDraw = false;
+      }
+    }
+
+    return (
+      <div>
+        {everything}
+      </div>
+    )
+  }
+}
+
+export default AllLists;
--- wishliste/src/App.js
+++ wishliste/src/App.js
@@ -3,10 +3,115 @@ import React, {Component} from 'react';
 import './App.css';
 import axios from 'axios';
 import SignUp from './SignUp.js';
+import List from './List.js';
+import AllLists from './AllLists.js'
 
 class App extends Component {
   state = {
-    results: []
+    results: [],
+    lists: [
+      {
+        name: 'Courses to take',
+        items: [
+          {
+            checked: 'checked',
+            name: 'Math 42',
+          },
+          {
+            checked: 'unchecked',
+            name: 'CS 157A',
+          },
+          {
+            checked: 'unchecked',
+            name: 'CS 157B'
+          },
+          {
+            checked: 'checked',
+            name: 'CS 146',
+          },
+        ],
+      },
+      {
+        name: 'Games to play',
+        items: [
+          {
+            checked: 'checked',
+            name: 'Fire Emblem Three Houses',
+          },
+          {
+            checked: 'checked',
+            name: 'Disgaea 5',
+          },
+          {
+            checked: 'unchecked',
+            name: 'Pokemon Sword'
+          },
+          {
+            checked: 'unchecked',
+            name: 'The Last of Us 2',
+          },
+        ],
+      },
+      {
+        name: 'BBQ Stuff',
+        items: [
+          {
+            checked: 'checked',
+            name: 'Hot dogs',
+          },
+          {
+            checked: 'unchecked',
+            name: 'Paper plates',
+          },
+          {
+            checked: 'unchecked',
+            name: 'Hamburgers'
+          },
+          {
+            checked: 'unchecked',
+            name: 'Ketchup',
+          },
+        ],
+      },
+      {
+        name: 'Pokemon Plushies',
+        items: [
+          {
+            checked: 'unchecked',
+            name: 'Pikachu',
+          },
+          {
+            checked: 'unchecked',
+            name: 'Cyndaquil',
+          },
+          {
+            checked: 'checked',
+            name: 'Turtwig',
+          },
+        ]
+      },
+      {
+        name: 'Dr. Seuss books',
+        items: [
+          {
+            checked: 'unchecked',
+            name: 'The Cat in the Hat',
+          },
+          {
+            checked: 'unchecked',
+            name: 'The Lorax',
+          },
+          {
+            checked: 'checked',
+            name: 'Database Systems: The Complete Book',
+          },
+          {
+            checked: 'unchecked',
+            name: 'Green Eggs and Ham',
+          },
+        ],
+      },
+    ],
   };
 
   componentDidMount(){
@@ -19,23 +124,39 @@ class App extends Component {
   };
 
   render(){
+
     return (
 
       <div className="App">
         <div className="Header-bar">
           <div className="Header-text">WishListé</div>
-        </div>
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <div className="Container-head">
-            Group #7 3-tier architecture demo <br />
+          <div className="Social-container">
+            <div className="Social-user"></div>
+            <div className="Social-facebook"></div>
+            <div className="Social-twitter"></div>    
           </div>
-          <div className="Container-list">
-            {this.state.results}
+        </div>
+
+        <div className="Button-container">
+          <div className="New-list-button">new wishlist</div>
+          <div className="New-list-button">new surprise wishlist</div>
+          <div className="New-list-button">new todo list</div>
+        </div>
+
+        <AllLists allLists={this.state.lists} />
+
+        <header className="List-row">
+          <div className="List-container">
+            <div className="List-head">
+              3-tier architecture demo (just to show it still works)
+            </div>
+            <div className="List-items">
+              <ul>
+                <li className="unchecked">{this.state.results}</li>
+              </ul>
+            </div>
           </div>
         </header>
-
-        
 
         </div>
     );
