c52254505fe0bd6df116b1c60b76638e6c2ed4d5
Francisco Romero <francisco.romero@sjsu.edu>
2019-11-30 12:55:56 -0800

    Added list type to each list

--- api/getListswithItems.php
+++ api/getListswithItems.php
@@ -5,7 +5,7 @@
 
     $username = $_POST['username'];
 
-    $query = "SELECT list_id,name,description FROM list NATURAL JOIN account_has_list WHERE username='$username'";
+    $query = "SELECT list_id,name,description FROM list NATURAL JOIN account_has_lists WHERE username='$username'";
     $result = $conn->query($query);
     if(!$result) die ("Database access failed: " . $conn->error);
 
@@ -16,6 +16,32 @@
     {
         $result->data_seek($i);
         $row = $result->fetch_array(MYSQLI_ASSOC);
+		
+		$list_id = $row['list_id'];
+		
+		$surprise_query = "SELECT list_id FROM surprise_wishlist WHERE list_id='$list_id'";
+		$todo_query = "SELECT list_id FROM todo_list WHERE list_id='$list_id'";
+		
+		$surprise_result = $conn->query($surprise_query);
+		if(!$surprise_result) die ("Database access failed: " . $conn->error);
+		
+		$surprise_rows = $surprise_result->num_rows;
+		
+		$todo_result = $conn->query($todo_query);
+		if(!$todo_result) die ("Database access failed: " . $conn->error);
+		
+		$todo_rows = $todo_result->num_rows;
+		
+		if($surprise_rows === 1){
+			$row['type'] = 'surprise';
+		}
+		elseif($todo_rows === 1){
+			$row['type'] = 'todo';
+		}
+		else{
+			$row['type'] = 'wish';
+		}
+		
 		
 		$additional_query = "SELECT item_id,name,description,checked FROM item NATURAL JOIN list_has_items WHERE list_id='$row[list_id]'";
         $additional_result = $conn->query($additional_query);
