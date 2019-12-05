e25f975a595508ad7e59b1a2e8e15fe8efe6d5ae
Francisco Romero <francisco.romero@sjsu.edu>
2019-12-04 23:06:53 -0800

    Created reset password table and added a sql query

--- api/forgotPassword.php
+++ api/forgotPassword.php
@@ -13,6 +13,11 @@
 	$rows = $result->num_rows;
 
     if($rows === 1) {
+		$length = random_bytes('4');
+		$token = bin2hex($length);
+		$forgot_query = "INSERT INTO reset_password(username, token) VALUES ('$username', '$token')";
+		$forgot_result = $conn->query($forgot_query);
+		if(!$forgot_result) die ("Database access failed: " . $conn->error);
       echo 1; //username found - send email
     }else {
        echo 0; //username not found - error
